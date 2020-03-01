56cec0122e18f387b3fc862a56de8727253db6dc
Sam <samuel.dereb@gmail.com>
2020-03-01 02:35:59 -0500

    time

--- Assets/Counter.cs
+++ Assets/Counter.cs
@@ -11,7 +11,7 @@ public class Counter : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        timeLeft = 31;
+        timeLeft = 231;
         timeText.color = Color.green;
     }
 
